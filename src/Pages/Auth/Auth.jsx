826f600f301da4324cff8962bb2d718db7ea293a
ChandraUday <chandrauday222333@gmail.com>
2024-03-14 20:44:10 +0530

    Auth start

---
+++ src/Pages/Auth/Auth.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./Auth.css";
+
+const Auth = () => {
+
+  const [isSignup, setIsSignup] = useState(false);
+  
+  return (
+    <section className="auth-section">
+        
+    </section>
+  )
+}
+
+export default Auth
